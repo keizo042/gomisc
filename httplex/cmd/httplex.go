0539b9e4cb94cf20716fca8ca5bf517fed3ae946
Kouichi Nakanishi <keizo.bookman@gmail.com>
2016-09-30 00:29:44 +0900

    [httplex] init

---
+++ httplex/cmd/httplex.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	http "github.com/keizo042/gomisc/httplex/http"
+	lex "github.com/keizo042/gomisc/httplex/lex"
+	"net"
+)
+
+func main() {
+
+}
---
+++ httplex/http/http.go
@@ -0,0 +1,11 @@
+package http
+
+type Http struct {
+}
+
+type Header struct {
+}
+
+type Body struct {
+	body string
+}
